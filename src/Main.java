ea9a1adc1bbd868f752d2c51d4b780e3c9552cae
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-17 17:18:47 +0500

    7.10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите n: ");
+        int n = scanner.nextInt();
+
+        System.out.println("Двузначные числа, которые делятся на " + n + " или содержат цифру " + n + ":");
+
+        int num = 10;
+        while (num <= 99) {
+            if (num % n == 0 || num % 10 == n || num / 10 == n) {
+                System.out.println(num);
+            }
+            num++;
+        }
     }
 }
